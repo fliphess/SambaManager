08b2e72a0362e2ac82ac0b9d2270a3d2516cdff4
Flip <flip@byte.nl>
2014-08-16 05:11:57 +0200

    boilerplate for server_control section

--- samba_manager/urls.py
+++ samba_manager/urls.py
@@ -4,20 +4,20 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^samba_manager/?$', 'samba_manager.views.index', name='samba_manager'),
+    url(r'^/?$', 'samba_manager.views.index', name='samba_manager'),
 
-    url(r'^samba_manager/list_users/$', 'samba_manager.views.list_users', name='list_users'),
-    url(r'^samba_manager/list_groups/$', 'samba_manager.views.list_groups', name='list_groups'),
-    url(r'^samba_manager/list_shares/$', 'samba_manager.views.list_shares', name='list_shares'),
+    url(r'^list_users/$', 'samba_manager.views.list_users', name='list_users'),
+    url(r'^list_groups/$', 'samba_manager.views.list_groups', name='list_groups'),
+    url(r'^list_shares/$', 'samba_manager.views.list_shares', name='list_shares'),
 
-    url(r'^samba_manager/add_user/$', 'samba_manager.views.add_user', name='add_user'),
-    url(r'^samba_manager/edit_user/(?P<id>\d+)/$', 'samba_manager.views.edit_user', name='edit_user'),
-    url(r'^samba_manager/delete_user/(?P<id>\d+)/$', 'samba_manager.views.delete_user', name='delete_user'),
+    url(r'^add_user/$', 'samba_manager.views.add_user', name='add_user'),
+    url(r'^add_group/$', 'samba_manager.views.add_group', name='add_group'),
+    url(r'^add_share/$', 'samba_manager.views.add_share', name='add_share'),
 
-    url(r'^samba_manager/add_group/$', 'samba_manager.views.add_group', name='add_group'),
-    url(r'^samba_manager/delete_group/(?P<id>\d+)/$', 'samba_manager.views.delete_group', name='delete_group'),
+    url(r'^edit_user/(?P<id>\d+)/$', 'samba_manager.views.edit_user', name='edit_user'),
+    url(r'^edit_share/(?P<id>\d+)/$', 'samba_manager.views.edit_share', name='edit_share'),
 
-    url(r'^samba_manager/add_share/$', 'samba_manager.views.add_share', name='add_share'),
-    url(r'^samba_manager/edit_share/(?P<id>\d+)/$', 'samba_manager.views.edit_share', name='edit_share'),
-    url(r'^samba_manager/delete_share/(?P<id>\d+)/$', 'samba_manager.views.delete_share', name='delete_share'),
+    url(r'^delete_user/(?P<id>\d+)/$', 'samba_manager.views.delete_user', name='delete_user'),
+    url(r'^delete_group/(?P<id>\d+)/$', 'samba_manager.views.delete_group', name='delete_group'),
+    url(r'^delete_share/(?P<id>\d+)/$', 'samba_manager.views.delete_share', name='delete_share'),
 )
--- server_control/urls.py
+++ server_control/urls.py
@@ -4,5 +4,13 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^server_control/?$', 'server_control.views.index', name='server_control'),
+    url(r'^/?$', 'server_control.views.index', name='server_control'),
+
+    url(r'^overview/$', 'server_control.views.overview', name='control_overview'),
+    url(r'^list_commands/$', 'server_control.views.list_commands', name='list_commands'),
+
+
+    url(r'^add_command/$', 'server_control.views.add_command', name='add_command'),
+    url(r'^edit_command/(?P<name>\w+)/$', 'server_control.views.edit_command', name='edit_command'),
+    url(r'^delete_command/(?P<name>\w+)/$', 'server_control.views.delete_command', name='delete_command'),
 )
